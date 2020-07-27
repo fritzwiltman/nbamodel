592aa602ece991ee0ffff6cf5513ae95caae4115
Fritz Wiltman <fgwilt1@gmail.com>
2020-07-27 17:28:04 -0400

    updated emailsender

--- emailsender.py
+++ emailsender.py
@@ -11,8 +11,8 @@ import pandas as pd
 def create_and_send_email(): 
     fromaddr = "drewpeacockslocks@gmail.com"
     toAddr = ""
-    bcc = ['fgwilt1@gmail.com', 'drewpeacockslocks@gmail.com', "fwiltman@umd.edu"]
-    # recepients = ["air.land96@gmail.com", "mbrdgrs6@gmail.com", "grant.abrams1@gmail.com", "ken.newmeyer@gmail.com", "jrwilt3@icloud.com", "benborucki13@gmail.com",  "jtoom13@gmail.com", "jacklombardo17@gmail.com", "rileycollins8244@gmail.com"]
+    # bcc = ["fgwilt1@gmail.com", "jwollensak@outlook.com", "vrwilt1@gmail.com", "air.land96@gmail.com", "mbrdgrs6@gmail.com", "grant.abrams1@gmail.com", "ken.newmeyer@gmail.com", "jrwilt3@icloud.com", "benborucki13@gmail.com",  "jtoom13@gmail.com", "jacklombardo17@gmail.com", "rileycollins8244@gmail.com"]
+    bcc = ["fgwilt1@gmail.com"]
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toAddr
@@ -44,7 +44,6 @@ def create_html_body():
             next_game = data["due_for_win"][team][1][0]
             next_opponent = data["due_for_win"][team][1][1]
             if next_opponent in data["due_for_win"]:
-<<<<<<< HEAD
                 formatted_team = "<li>{} has lost {} in a row, but playing {} who is also due for a win. <br>Recommended wager: <strong>Fade this game</strong>.</li><br>"\
                     .format(team, data["due_for_win"][team][0], next_opponent)
             elif next_opponent in data["due_for_loss"]:
@@ -52,12 +51,6 @@ def create_html_body():
                 formatted_team = "<li>{} has lost {} in a row.<br>Their next opponent, {}, has won {} in a row and is due for a loss.<br>{}.<br>{}.</li><br>"\
                     .format(team, data["due_for_win"][team][0], next_opponent, next_opp_losses, \
                     (get_recommended_units_for_double_bet(int(data["due_for_win"][team][0]), next_opp_losses)), next_game)
-=======
-                formatted_team = "<li>{} has lost {} in a row, but playing {} who is also due for a win.<br>Recommended wager: <strong>Fade this game</strong>.</li><br>".format(team, data["due_for_win"][team][0], next_opponent)
-            elif next_opponent in data["due_for_loss"]:
-                next_opp_losses = int(data["due_for_loss"][next_opponent][0])
-                formatted_team = "<li>{} has lost {} in a row.<br>Their next opponent, {}, has won {} in a row and is due for a loss.<br>{}.<br>{}.</li><br>".format(team, data["due_for_win"][team][0], next_opponent, next_opp_losses, (get_recommended_units_for_double_bet(int(data["due_for_win"][team][0]), next_opp_losses)), next_game)
->>>>>>> parent of e012472... added absolute paths to all file openings
             else:
                 formatted_team = "<li>{} has lost {} in a row.<br>{}.<br>{}.</li><br>".format(team, data["due_for_win"][team][0], get_recommended_units(int(data["due_for_win"][team][0])), next_game)
             teams_due_for_win.append(formatted_team) 
@@ -66,7 +59,6 @@ def create_html_body():
             next_game = data["due_for_loss"][team][1][0]
             next_opponent = data["due_for_loss"][team][1][1]
             if next_opponent in data["due_for_loss"]:
-<<<<<<< HEAD
                 formatted_team = "<li>{} has won {} in a row, but playing {} who is also due for a loss. <br>Recommended wager: <strong>Fade this game</strong>.</li><br>"\
                     .format(team, data["due_for_loss"][team][0], next_opponent)
             elif next_opponent in data["due_for_win"]:
@@ -74,21 +66,32 @@ def create_html_body():
                 formatted_team = "<li>{} has won {} in a row.<br>Their next opponent, {}, has lost {} in a row and is due for a win.<br>Recommended wager: \
                     Wager is listed above in <strong>\"Teams due for win\"</strong> list under {}.<br>{}.</li><br>"\
                     .format(team, data["due_for_loss"][team][0], next_opponent, next_opp_losses, next_opponent, next_game)
-=======
-                formatted_team = "<li>{} has won {} in a row, but playing {} who is also due for a loss.<br>Recommended wager: <strong>Fade this game</strong>.</li><br>".format(team, data["due_for_loss"][team][0], next_opponent)
-            elif next_opponent in data["due_for_win"]:
-                next_opp_losses = int(data["due_for_win"][next_opponent][0])
-                formatted_team = "<li>{} has won {} in a row.<br>Their next opponent, {}, has lost {} in a row and is due for a win.<br>Recommended wager: Wager is listed above in <strong>\"Teams due for win\"</strong> list under {}.<br>{}.</li><br>".format(team, data["due_for_loss"][team][0], next_opponent, next_opp_losses, next_opponent, next_game)
->>>>>>> parent of e012472... added absolute paths to all file openings
             else:
                 formatted_team = "<li>{} has won {} in a row.<br>{}.<br>{}.</li><br>".format(team, data["due_for_loss"][team][0], get_recommended_units(int(data["due_for_loss"][team][0])), next_game)
             teams_due_for_loss.append(formatted_team)
+
+    no_games_prompt = ""
+    no_games_today_win = True
+    no_games_today_loss = True
+    for line in teams_due_for_win:
+        if "TODAY" in line:
+            no_games_today_win = False
+            break
+    for line in teams_due_for_loss:
+        if "TODAY" in line:
+            no_games_today_loss = False
+            break
+    if no_games_today_win and no_games_today_loss:
+        print("No games today!")
+        no_games_prompt = "<h2 style=\"color:red; text-align:center\"><i><strong><u>There are no games scheduled for today that I recommend wagering.</u></strong></i></h2><br>"
+
 
     html = """\
     <html>
     <head></head>
     <body>
         <h1 style="text-align:center">Drew Peacock's NBA Locks</h1><br>
+        {}
         <h2>Teams due for a win</h2>
         <ul>
             {}
@@ -106,7 +109,7 @@ def create_html_body():
         </ul> -->
     </body>
     </html>
-    """.format(sort_games(teams_due_for_win), sort_games(teams_due_for_loss))
+    """.format(no_games_prompt, sort_games(teams_due_for_win), sort_games(teams_due_for_loss))
 
     return html
 
@@ -134,6 +137,6 @@ def get_recommended_units_for_double_bet(consecutive_outcomes1, consecutive_outc
     recommended_units_string = "Recommended wager: <strong>(Double Wager) </strong>"
     total_outcomes = consecutive_outcomes1 + consecutive_outcomes2
     if (total_outcomes <= 7): recommended_units_string += "2 units"
-    elif (8 < total_outcomes <= 10): recommended_units_string += "3 units"
+    elif (7 < total_outcomes <= 10): recommended_units_string += "3 units"
     else: recommended_units_string += "4 units"
     return recommended_units_string
